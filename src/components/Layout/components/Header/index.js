baea62ce2d9437471335d966d3ce0fa92e1f1da9
Bingao9x <huynhconghuu971993@gmail.com>
2022-07-06 02:14:56 +0700

    Xây dựng UI phần Header #3

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -4,10 +4,11 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircleXmark, faMagnifyingGlass, faSpinner } from '@fortawesome/free-solid-svg-icons';
 import Tippy from '@tippyjs/react/headless';
 
+import Button from '~/components/Button';
 import { Wrapper as PopperWrapper } from '~/components/Popper';
 import styles from './Header.module.scss';
 import images from '~/assets/images';
-import AccountItem from '../AccountItem';
+import AccountItem from '~/components/AccountItem';
 
 const cx = classNames.bind(styles);
 function Header() {
@@ -51,6 +52,10 @@ function Header() {
                     </div>
                 </Tippy>
                 <div className={cx('actions')}></div>
+                <div className={cx('actions')}>
+                    <Button text>Upload</Button>
+                    <Button primary>Log in</Button>
+                </div>
             </div>
         </header>
     );
